792d6fb06a97f38698add676430a2f54b04fe3eb
Venkat Satya Uday Dhanush Karri <nie24@txstate.edu>
2025-08-22 15:53:21 -0500

    adding an edit feature,converting string into number, replacing a string with another for edit feature, list indexing to edit.

--- Python_Applications_Projects/PyApp_1_Todo.py/main.py
+++ Python_Applications_Projects/PyApp_1_Todo.py/main.py
@@ -1,16 +1,22 @@
 todos = []                               # declaring todos as a list
 
 while True:                             #using while loop
-    user_action = input("Type add or show or exit: ")     # giving user input
+    user_action = input("Type add or show or edit or exit: ")     # giving user input
     user_action = user_action.strip()   # strip() is used for removing trailing spaces
 
     match user_action:                  #match - case
         case "add":
             todo = input("Enter a todo:")
-            todos.append(todo.title())     #used for add input values to the todos list and title is used for capital letters of every word
+            todos.append(todo)     #used for add input values to the todos list and title is used for capital letters of every word
         case "show":
             for item in todos:       #using for loop
-                print(item)
+                print(item.capitalize())      #showing the list of items and and capitalising too
+        case "edit":                  #editing a list item
+            number = input("Enter the number of todo to edit:")
+            print(number)
+            new_todo = input("Enter your todo to edit:")    #replacing the required item into the list
+            todos[int(number)] = new_todo # converting string with int
+            print(new_todo)
         case "exit":                 #breaking the while loop under match case
             break
 
@@ -18,7 +24,7 @@ print("bye")                        # prints for while loop after exit
 
 print("To do added successfully!")
 
-print("Number of todos:", len(todos))   #checks the length of the todos i.e. the no. of items in the list
+print("Number of todos: ", len(todos))   #checks the length of the todos i.e. the no. of items in the list
 
 
 
