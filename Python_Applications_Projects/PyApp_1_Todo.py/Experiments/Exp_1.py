ffee3e53ef7f78573dcdbd396729c8c6432ac2f1
Venkat Satya Uday Dhanush Karri <nie24@txstate.edu>
2025-08-22 14:50:58 -0500

    Modifications

--- Python_Applications_Projects/PyApp_1_Todo.py/Experiments/Exp_1.py
+++ Python_Applications_Projects/PyApp_1_Todo.py/Experiments/Exp_1.py
@@ -1,5 +1,20 @@
+todos = []
 
+while True:  #using while loop
+    user_action = input("Type add or show or exit: ")  # giving user input
+    user_action = user_action.strip()  # strip() is used for removing trailing spaces
 
-while True:
-    name = input("Enter your name:")
-    print(name.capitalize())
+    match user_action:  #match - case
+        case "add":
+            todo = input("Enter a todo:")
+            todos.append(
+                todo.title())                                  #used for add input values to the todos list and title is used for capital letters of every word
+        case "show" | "display":                                 #using a bitwsie  OR operator
+            for item in todos:                                     #using for loop
+                print(item.title())                                 #making every eord capital
+        case "exit":                                               #breaking the while loop under match case
+            break
+        case whatever:                                              #using a whatever
+            print("Unknown command")
+
+print("bye")
