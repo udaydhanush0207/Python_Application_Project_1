66f37c15e2fb902aa34f3b08b2f1400e4161f8e1
Venkat Satya Uday Dhanush Karri <nie24@txstate.edu>
2025-08-22 15:53:50 -0500

    usage of for-loops

--- Python_Applications_Projects/PyApp_1_Todo.py/bonus_3.py
+++ Python_Applications_Projects/PyApp_1_Todo.py/bonus_3.py
@@ -1,6 +1,10 @@
 meals = ["pasta","pizza","salad"]
 
-for meal in meals:   # for loops in general run over strings and as well as lists and unlike while they exit when their iterations are satisfied
+for meal in meals:   # The for-loop executes the indented lines n-times, where n is the number of items in the list that is being iterated.
     print(meal.capitalize())
 
 print("Bye")
+
+
+#A while loop iterates as long as the while-condition is True.
+#A for-loop iterates that many times as there are items in the list that is being iterated.
